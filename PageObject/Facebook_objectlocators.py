d255f92d30bb2a90168cc522d3ca7cd73f9ea15b
Bhanu Prakash <bhanuprakash.kollati03@gmail.com>
2021-08-16 16:13:40 +0530

    adding files

---
+++ PageObject/Facebook_objectlocators.py
@@ -0,0 +1,16 @@
+"""
+
+"""
+
+
+class FacebookLogin:
+    """
+
+    """
+    username_xpath = '//input[@name="email"]'
+    password_xpath = '//input[@name="pass"]'
+    login_btn_xpath = '//button[@name="login"]'
+
+# class FacebookPost:
+#     post_text_xpath = ''
+#     post_btn_xpath = ''
